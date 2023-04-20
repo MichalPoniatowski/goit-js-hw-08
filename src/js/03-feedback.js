04254ba76cb900084cec41c039fc166bdfcbab2a
MichalPoniatowski <m.poniatowski87@gmail.com>
2023-04-20 10:06:56 +0200

    JS files corrected

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,31 +1,23 @@
-import throttle from "lodash.throttle";
+import throttle from 'lodash.throttle';
 
-const formEl = document.querySelector(".feedback-form");
 const btnEl = document.querySelector('button[type="submit"]');
 const emailEl = document.querySelector('input[type="email"]');
 const messageEl = document.querySelector('textarea[name="message"]');
 
-console.log(formEl);
-console.log(btnEl);
-console.log(emailEl);
-console.log(messageEl);
-
 let data = {
-  email: "",
-  message: "",
+  email: '',
+  message: '',
 };
 
 const checkLocalStorage = () => {
-  const savedData = JSON.parse(localStorage.getItem("feedback-form-state"));
-  console.log(savedData);
+  const savedData = JSON.parse(localStorage.getItem('feedback-form-state'));
 
   if (savedData) {
     emailEl.value = savedData.email;
     messageEl.value = savedData.message;
   } else {
-    emailEl.value = "";
-    messageEl.value = "";
-    console.log("pusto");
+    emailEl.value = '';
+    messageEl.value = '';
   }
 };
 
@@ -33,18 +25,12 @@ checkLocalStorage();
 
 function emailLocalStorage(event) {
   data.email = event.target.value;
-  localStorage.setItem("feedback-form-state", JSON.stringify(data));
+  localStorage.setItem('feedback-form-state', JSON.stringify(data));
 }
 
 function messageLocalSorage(event) {
   data.message = event.target.value;
-  // TO NIE DZIAŁA:
-
-  // throttle(() => {
-  //   localStorage.setItem("feedback-form-state", JSON.stringify(data));
-  // }, 5000);
-
-  localStorage.setItem("feedback-form-state", JSON.stringify(data));
+  localStorage.setItem('feedback-form-state', JSON.stringify(data));
 }
 
 function resetLocalSorage(event) {
@@ -53,7 +39,6 @@ function resetLocalSorage(event) {
   checkLocalStorage();
 }
 
-// TO DZIAŁA:
-emailEl.addEventListener("input", throttle(emailLocalStorage, 1000));
-messageEl.addEventListener("input", throttle(messageLocalSorage, 1000));
-btnEl.addEventListener("click", resetLocalSorage);
+emailEl.addEventListener('input', throttle(emailLocalStorage, 1000));
+messageEl.addEventListener('input', throttle(messageLocalSorage, 1000));
+btnEl.addEventListener('click', resetLocalSorage);
